f9590f8b29243214196119096c79bfdec4ae778d
Akhil Wali <akhil.wali.10@gmail.com>
2022-04-04 18:11:49 +1200

    Add missing packages

--- packages.el
+++ packages.el
@@ -1,6 +1,8 @@
 ;;; ~/.doom.d/packages.el -*- no-byte-compile: t; -*-
 
 (package! cider-eval-sexp-fu)
+(package! eval-sexp-fu)
+(package! helm-ls-git)
 (package! flycheck-joker :disable t)
 
 (when IS-WINDOWS
